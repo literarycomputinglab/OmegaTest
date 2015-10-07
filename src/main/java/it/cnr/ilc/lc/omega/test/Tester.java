5d8de56a41e914dd23c7c2053477d2a3ad2e9aa7
angelo <angelo@dylan2>
2015-10-07 22:24:25 +0200

    TextLocus clone

--- src/main/java/it/cnr/ilc/lc/omega/test/Tester.java
+++ src/main/java/it/cnr/ilc/lc/omega/test/Tester.java
@@ -21,10 +21,24 @@ public class Tester {
     private static final Long SOURCE_ID = 7l;
     private static final Long ANNOTATION_ID = 9l;
 
-    public static final String CONTENT_PAGE1_LETTER1 = "Contente page 1 letter 1";
-    public static final String CONTENT_PAGE2_LETTER1 = "Contente page 2 letter 1";
-    public static final String CONTENT_PAGE1_LETTER2 = "Contente page 1 letter 2";
-    public static final String CONTENT_PAGE2_LETTER2 = "Contente page 2 letter 2";
+    public static final String CONTENT_PAGE1_LETTER1 = "Botvitus Nericius Christophoro Clavio Salutem " +
+"Occupationibus tuis Doctissime Clavi parcens, in tertia mea ad te epistola, praesertim vero in secunda illius parte, quae erat de puncto lineae Dinostrati ultimo abs te invento; brevior fortasse fui, quam oportuit, ideoque hac scriptione rem totam paullo uberius, si potero, explicabo. " +
+"Principio igitur lineae conchoidis accidens certissimum est, nimirum, si per lineam Dinostrati transeat, eam per lineas rectas dividere in partes supra infraque aequales, propter motum lineae a summo, velut polo, basim permeantis; qui quidem motus familiam in hoc negotio, vel in primis ducit. " +
+"Certum quoque est punctum, quod aequidistantiae dicitur nempe g esse in linea conchoide, utpote ipsam describens. " +
+"Esse autem illud idem punctum g in linea Dinostrati, ex sola positione, sine ulla ratione geometrica discimus. " +
+"age enim punctum illud, estne certa proportione descriptum, an sit extempore notatum, ut fieri solet, lineae complendae caussa? ";
+
+    public static final String CONTENT_PAGE2_LETTER1 = "Si huius generis est, iam constat esse mechanicum, et a nostro instituto alienum; sin illius, cur non demonstratur? " +
+"linea etenim Dinostrati, nequaquam, ne secundum nota quidem puncta geometrice sine demonstratione, describi potest, nedum secundum ignota, cuiusmodi est punctum e hoc est, punctum g in tot aetatum controversia positum. " +
+"Nam quod ad tres quadrantes attinet, concedo omnes esse unum et eundem, sed nego ullum esse quadrantem medium, nisi prius ostendatur punctum g esse in linea iam dicta. " +
+"ex quo sequitur nondum penitus adsolutum esse libellum de inventione puncti ultimi quod tum scripsi; quamvis a perfectione non longe absit verum ego de securitate geometrica loquor. ";
+
+    public static final String CONTENT_PAGE1_LETTER2 = "Quia si, ut ratio naturalis docet, propter quod unum quodque tale fuerit illud magis est tale; et punctum g ut iam docui, est incertum, multo incertius erit punctum cum huius veritas ex illius veritate dependeat. " +
+"Quamobrem tuarum partium est, optime magister, quoniam praeclaram hanc segetem tanta dexteritate sevisti, et rigasti, tandem ad maturitatem perductam, humano genere invisendam communices. " +
+"bene vale; et nobis, tum de his tum etiam de Ubaldi novitate, quod commodo tuo fiat, quamprimum responde. " +
+"Mantua Carpentaniae pridie idus Septembris. M. D. LXXXXIIX. ";
+    public static final String CONTENT_PAGE2_LETTER2 = "Tuus, more solito, Botvitus Nericius de Sala, gothus manu propria " +
+"religiosissimo et doctissimo viro, Christophoro Clavio Societatis Iesu manu propria Romam manu propria ";
 
     public static final String BASE = "BaseExtension";
     public static final String NOTA = "il contenuto di una nota";
@@ -49,166 +63,6 @@ public class Tester {
         /* test builder */
 //        tester.test6();
         tester.test7();
-
-    }
-
-    private void test1() {
-        Session session = Neo4jSessionFactory.getNeo4jSession();
-        session.beginTransaction();
-
-        Source<TextContent> source = Source.sourceOf(TextContent.class);
-        source.setContent(new TextContent());
-        source.getContent().setText(Tester.CONTENT_PAGE1_LETTER1);
-        session.save(source);
-
-        session.getTransaction().commit();
-    }
-
-    /**/
-    private void test2() {
-        Session session = Neo4jSessionFactory.getNeo4jSession();
-        session.beginTransaction();
-
-        Source source = session.load(Source.class, SOURCE_ID);
-
-        TextLocus locus = new TextLocus();
-        locus.setSource(source);
-        locus.setStart(5);
-        locus.setEnd(10);
-
-        Annotation.register(Tester.BASE, BaseAnnotationExtension.class); //MODIFICARE in (TIPO, CLASSE)
-        Annotation<TextContent, BaseAnnotationExtension> nota = Annotation.newAnnotation(Tester.BASE, new BaseExtensionBuilder().field1("nota"));
-
-        System.err.println(nota.toString());
-//
-//        BaseExtension ex = nota.getExtension();
-//        ex.setField1("nota");
-//          List<Locus> loci = new ArrayList<Locus>();
-        //        locus.setAnnotation(nota);
-        //        loci.add(locus);
-        locus.setAnnotation(nota);
-        nota.addLocus(locus);
-
-        session.save(nota);
-        session.getTransaction().commit();
-
-        //        Note note = new Note();
-        //        note.setField1("Ciao");
-        //        note.setField2("Bello");
-        //
-        //        Annotation<Note> annotation = new Annotation<>();
-        //        annotation.setExtension(note);
-        //        annotation.setLocus(new ArrayList<Locus>());
-        //        annotation.getLocus().add(locus);
-        //        
-        //        locus.setAnnotation(annotation);
-        //
-        //        session.save(annotation);
-        //
-        //        session.getTransaction().commit();
-    }
-
-    private void test2_1() {
-        Session session = Neo4jSessionFactory.getNeo4jSession();
-        session.beginTransaction();
-
-        // load ha bisogno di riparametrizzare i generics
-        Annotation<TextContent, BaseAnnotationExtension> annotation = session.load(Annotation.class, ANNOTATION_ID);
-        TextContent content = new TextContent();
-
-        content.setText(Tester.TESTO);
-        annotation.setContent(content);
-
-        session.save(annotation);
-        session.getTransaction().commit();
-
-    }
-    /**/
-
-    private void test3() {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-
-        System.err.println("in test tre");
-        Session session = Neo4jSessionFactory.getNeo4jSession();
-        session.beginTransaction();
-
-        Annotation<TextContent, BaseAnnotationExtension> annotation = session.load(Annotation.class, ANNOTATION_ID, 5);
-
-        System.err.println(annotation.toString());
-
-        TextLocus locus = new TextLocus();
-        Locus l = annotation.getLoci().next();
-        Source source = l.getSource();
-        System.err.println("source " + source.toString());
-        System.err.println("source.getcontent (" + source.getContent() + ")");
-        TextContent content = (TextContent) source.getContent();
-        System.err.println("content: " + content.getText());
-        locus.setSource(source);
-        locus.setAnnotation(annotation);
-        locus.setStart(6);
-        locus.setEnd(12);
-        //        
-        annotation.addLocus(locus);
-        session.save(annotation);
-        session.getTransaction().commit();
-
-        //        session.getTransaction().close();
-    }
-    /**/
-
-    private void test4() {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-
-        System.err.println("in test 4");
-        Session session = Neo4jSessionFactory.getNeo4jSession();
-        session.beginTransaction();
-        //
-        Annotation annotation = session.load(Annotation.class, ANNOTATION_ID);
-
-        Annotation.register(Tester.BASE, BaseAnnotationExtension.class); //MODIFICARE in (TIPO, CLASSE)
-        Annotation<TextContent, BaseAnnotationExtension> nota2 = Annotation.newAnnotation(Tester.BASE, new BaseExtensionBuilder().field1("nota2 che annota nota"));
-
-//        BaseExtension bex = nota2.getExtension();
-//
-//        bex.setField1("nota2 che annota nota");
-//        Relation<BaseRelationExtension> rel = new Relation<>();
-//        rel.setSourceAnnotation(nota2);
-//        rel.setTargetAnnotation(annotation);
-//        BaseRelationExtension bre = new BaseRelationExtension();
-//        bre.setField1("base type relation");
-//        rel.setExtension(bre);
-//        nota2.addRelation(rel);
-//        nota2.getRelations().add(rel);
-        session.save(nota2);
-        session.getTransaction().commit();
-
-    }
-    /**/
-
-    private void test5() {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-
-        System.err.println("in test 5");
-        Session session = Neo4jSessionFactory.getNeo4jSession();
-        session.beginTransaction();
-
-        Annotation<TextContent, BaseAnnotationExtension> annotationTarget = session.load(Annotation.class, 1l);
-        System.err.println("content " + annotationTarget.getContent().getText());
-        Annotation.register(Tester.BASE, BaseAnnotationExtension.class); //MODIFICARE in (TIPO, CLASSE)
-        Annotation<TextContent, BaseAnnotationExtension> annotationSource = Annotation.newAnnotation(Tester.BASE, new BaseExtensionBuilder().field1("nota che annota il content di una nota"));
-
-//        BaseExtension bex = annotationSource.getExtension();
-//        bex.setField1("nota che annota il content di una nota");
-        TextLocus locus = new TextLocus();
-
-        locus.setSource(annotationTarget);
-        locus.setAnnotation(annotationSource);
-        locus.setStart(7);
-        locus.setEnd(20);
-        annotationSource.addLocus(locus);
-
-        session.save(annotationSource);
-        session.getTransaction().commit();
 
     }
 
@@ -269,9 +123,9 @@ public class Tester {
         sourcePage1Letter1.getContent().setText(Tester.CONTENT_PAGE1_LETTER1);
         TextLocus locus = Locus.locusOf(TextLocus.class, Locus.PointsTo.SOURCE);
         locus.setSource(sourcePage1Letter1);
-        Annotation<TextContent, FileAnnotationExtension> page1Letter1 = Annotation.newAnnotation("File", new FileBuilder().name("Pagina 1 Letter 1"));
-        page1Letter1.addLocus(locus);
+        Annotation<TextContent, FileAnnotationExtension> page1Letter1 = Annotation.newAnnotation("File", new FileBuilder().name("Source 1"));
         locus.setAnnotation(page1Letter1);
+        page1Letter1.addLocus(locus);
         session.save(page1Letter1);
 
         Source<TextContent> sourcePage2Letter1 = Source.sourceOf(TextContent.class);
@@ -279,9 +133,10 @@ public class Tester {
         sourcePage2Letter1.getContent().setText(Tester.CONTENT_PAGE2_LETTER1);
         locus = Locus.locusOf(TextLocus.class, Locus.PointsTo.SOURCE);
         locus.setSource(sourcePage2Letter1);
-        Annotation<TextContent, FileAnnotationExtension> page2Letter1 = Annotation.newAnnotation("File", new FileBuilder().name("Pagina 2 Letter 1"));
-        page2Letter1.addLocus(locus);
+        Annotation<TextContent, FileAnnotationExtension> page2Letter1 = Annotation.newAnnotation("File", new FileBuilder().name("Source 2"));
         locus.setAnnotation(page2Letter1);
+        page2Letter1.addLocus(locus);
+
         session.save(page2Letter1);
 
         Source<TextContent> sourcePage1Letter2 = Source.sourceOf(TextContent.class);
@@ -289,9 +144,10 @@ public class Tester {
         sourcePage1Letter2.getContent().setText(Tester.CONTENT_PAGE1_LETTER2);
         locus = Locus.locusOf(TextLocus.class, Locus.PointsTo.SOURCE);
         locus.setSource(sourcePage1Letter2);
-        Annotation<TextContent, FileAnnotationExtension> page1Letter2 = Annotation.newAnnotation("File", new FileBuilder().name("Pagina 1 Letter 2"));
-        page1Letter2.addLocus(locus);
+        Annotation<TextContent, FileAnnotationExtension> page1Letter2 = Annotation.newAnnotation("File", new FileBuilder().name("Source 3"));
         locus.setAnnotation(page1Letter2);
+
+        page1Letter2.addLocus(locus);
         session.save(page1Letter2);
 
         Source<TextContent> sourcePage2Letter2 = Source.sourceOf(TextContent.class);
@@ -299,12 +155,12 @@ public class Tester {
         sourcePage2Letter2.getContent().setText(Tester.CONTENT_PAGE2_LETTER2);
         locus = Locus.locusOf(TextLocus.class, Locus.PointsTo.SOURCE);
         locus.setSource(sourcePage2Letter2);
-        Annotation<TextContent, FileAnnotationExtension> page2Letter2 = Annotation.newAnnotation("File", new FileBuilder().name("Pagina 2 Letter 2"));
-        page2Letter2.addLocus(locus);
+        Annotation<TextContent, FileAnnotationExtension> page2Letter2 = Annotation.newAnnotation("File", new FileBuilder().name("Source 4"));
         locus.setAnnotation(page2Letter2);
-        session.save(page2Letter2);
 
-       
+        page2Letter2.addLocus(locus);
+
+        session.save(page2Letter2);
 
         Folder archive = new Folder();
         archive.setName("Archive");
