1428fdc3b5337bc6c9c5bdec41982224381f849c
oakgen <oakgen@macalbanesi.ilc.cnr.it>
2015-10-07 16:56:57 +0200

    Refactor tester

--- src/main/java/it/cnr/ilc/lc/omega/test/Tester.java
+++ src/main/java/it/cnr/ilc/lc/omega/test/Tester.java
@@ -1,5 +1,8 @@
 package it.cnr.ilc.lc.omega.test;
 
+import it.cnr.ilc.lc.omega.clavius.catalog.FileBuilder;
+import it.cnr.ilc.lc.omega.clavius.catalog.entity.Folder;
+import it.cnr.ilc.lc.omega.clavius.catalog.entity.FileAnnotationExtension;
 import it.cnr.ilc.lc.omega.entity.Annotation;
 import it.cnr.ilc.lc.omega.entity.Content;
 import it.cnr.ilc.lc.omega.entity.Locus;
@@ -18,7 +21,11 @@ public class Tester {
     private static final Long SOURCE_ID = 7l;
     private static final Long ANNOTATION_ID = 9l;
 
-    public static final String TEXTCONTENT = "Il contenuto della risorsa principale";
+    public static final String CONTENT_PAGE1_LETTER1 = "Contente page 1 letter 1";
+    public static final String CONTENT_PAGE2_LETTER1 = "Contente page 2 letter 1";
+    public static final String CONTENT_PAGE1_LETTER2 = "Contente page 1 letter 2";
+    public static final String CONTENT_PAGE2_LETTER2 = "Contente page 2 letter 2";
+
     public static final String BASE = "BaseExtension";
     public static final String NOTA = "il contenuto di una nota";
     public static final String TESTO = "un contenuto generico";
@@ -40,7 +47,8 @@ public class Tester {
         /* crea annotazione di content di annotazione */
         //tester.test5();
         /* test builder */
-        tester.test6();
+//        tester.test6();
+        tester.test7();
 
     }
 
@@ -50,7 +58,7 @@ public class Tester {
 
         Source<TextContent> source = Source.sourceOf(TextContent.class);
         source.setContent(new TextContent());
-        source.getContent().setText(Tester.TEXTCONTENT);
+        source.getContent().setText(Tester.CONTENT_PAGE1_LETTER1);
         session.save(source);
 
         session.getTransaction().commit();
@@ -211,11 +219,11 @@ public class Tester {
 
         Source<TextContent> source = Source.sourceOf(TextContent.class);
         source.setContent(Content.contentOf(TextContent.class));
-        source.getContent().setText(Tester.TEXTCONTENT);
+        source.getContent().setText(Tester.CONTENT_PAGE1_LETTER1);
         session.save(source);
 
-        TextLocus locus = Locus.locusOf(TextLocus.class, Locus.PointTo.SOURCE);
-       // TextLocus locus = new TextLocus();
+        TextLocus locus = Locus.locusOf(TextLocus.class, Locus.PointsTo.CONTENT);
+        // TextLocus locus = new TextLocus();
         locus.setSource(source);
 
         locus.setStart(5);
@@ -248,6 +256,78 @@ public class Tester {
         session.save(nota2);
         session.getTransaction().commit();
 
+    }
+
+    private void test7() {
+        Session session = Neo4jSessionFactory.getNeo4jSession();
+        session.beginTransaction();
+
+        Annotation.register("File", FileAnnotationExtension.class); //MODIFICARE in (TIPO, CLASSE)
+
+        Source<TextContent> sourcePage1Letter1 = Source.sourceOf(TextContent.class);
+        sourcePage1Letter1.setContent(Content.contentOf(TextContent.class));
+        sourcePage1Letter1.getContent().setText(Tester.CONTENT_PAGE1_LETTER1);
+        TextLocus locus = Locus.locusOf(TextLocus.class, Locus.PointsTo.SOURCE);
+        locus.setSource(sourcePage1Letter1);
+        Annotation<TextContent, FileAnnotationExtension> page1Letter1 = Annotation.newAnnotation("File", new FileBuilder().name("Pagina 1 Letter 1"));
+        page1Letter1.addLocus(locus);
+        locus.setAnnotation(page1Letter1);
+        session.save(page1Letter1);
+
+        Source<TextContent> sourcePage2Letter1 = Source.sourceOf(TextContent.class);
+        sourcePage2Letter1.setContent(Content.contentOf(TextContent.class));
+        sourcePage2Letter1.getContent().setText(Tester.CONTENT_PAGE2_LETTER1);
+        locus = Locus.locusOf(TextLocus.class, Locus.PointsTo.SOURCE);
+        locus.setSource(sourcePage2Letter1);
+        Annotation<TextContent, FileAnnotationExtension> page2Letter1 = Annotation.newAnnotation("File", new FileBuilder().name("Pagina 2 Letter 1"));
+        page2Letter1.addLocus(locus);
+        locus.setAnnotation(page2Letter1);
+        session.save(page2Letter1);
+
+        Source<TextContent> sourcePage1Letter2 = Source.sourceOf(TextContent.class);
+        sourcePage1Letter2.setContent(Content.contentOf(TextContent.class));
+        sourcePage1Letter2.getContent().setText(Tester.CONTENT_PAGE1_LETTER2);
+        locus = Locus.locusOf(TextLocus.class, Locus.PointsTo.SOURCE);
+        locus.setSource(sourcePage1Letter2);
+        Annotation<TextContent, FileAnnotationExtension> page1Letter2 = Annotation.newAnnotation("File", new FileBuilder().name("Pagina 1 Letter 2"));
+        page1Letter2.addLocus(locus);
+        locus.setAnnotation(page1Letter2);
+        session.save(page1Letter2);
+
+        Source<TextContent> sourcePage2Letter2 = Source.sourceOf(TextContent.class);
+        sourcePage2Letter2.setContent(Content.contentOf(TextContent.class));
+        sourcePage2Letter2.getContent().setText(Tester.CONTENT_PAGE2_LETTER2);
+        locus = Locus.locusOf(TextLocus.class, Locus.PointsTo.SOURCE);
+        locus.setSource(sourcePage2Letter2);
+        Annotation<TextContent, FileAnnotationExtension> page2Letter2 = Annotation.newAnnotation("File", new FileBuilder().name("Pagina 2 Letter 2"));
+        page2Letter2.addLocus(locus);
+        locus.setAnnotation(page2Letter2);
+        session.save(page2Letter2);
+
+       
+
+        Folder archive = new Folder();
+        archive.setName("Archive");
+
+        Folder fondo = new Folder();
+        fondo.setName("Fondo");
+        archive.addFolder(fondo);
+
+        Folder manuscript530 = new Folder();
+        manuscript530.setName("Manuscript 530");
+        fondo.addFolder(manuscript530);
+        manuscript530.addFile(page1Letter1);
+        manuscript530.addFile(page2Letter1);
+        manuscript530.addFile(page1Letter2);
+        manuscript530.addFile(page2Letter2);
+
+        Folder manuscript533 = new Folder();
+        manuscript533.setName("Manuscript 533");
+        fondo.addFolder(manuscript533);
+
+        session.save(archive);
+
+        session.getTransaction().commit();
     }
 
 }
