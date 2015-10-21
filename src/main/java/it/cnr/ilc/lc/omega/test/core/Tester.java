3c1edb306275980834f94d39d7c1eb17af06db76
oakgen <oakgen@macalbanesi.ilc.cnr.it>
2015-10-21 17:13:06 +0200

    microkernel

--- src/main/java/it/cnr/ilc/lc/omega/test/core/Tester.java
+++ src/main/java/it/cnr/ilc/lc/omega/test/core/Tester.java
@@ -5,10 +5,12 @@
  */
 package it.cnr.ilc.lc.omega.test.core;
 
-import it.cnr.ilc.lc.omega.core.DocumentManager;
+import it.cnr.ilc.lc.omega.core.ResourceManager;
 import it.cnr.ilc.lc.omega.core.OmegaCore;
+import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.activation.MimeType;
 import javax.activation.MimeTypeParseException;
 import sirius.kernel.di.std.Part;
 
@@ -17,25 +19,27 @@ import sirius.kernel.di.std.Part;
  * @author angelo
  */
 public class Tester {
+
     @Part
-    private static DocumentManager documentManager;
-    
-    public static void main(String[] argv){
-        OmegaCore.init(argv);
+    private static ResourceManager resourceManager;
+
+    public static void main(String[] argv) {
+        OmegaCore.start();
         System.err.println("Core Inizializzato");
         try {
             test();
         } catch (MimeTypeParseException ex) {
             Logger.getLogger(Tester.class.getName()).log(Level.SEVERE, null, ex);
         }
+        OmegaCore.stop();
     }
 
     private static void test() throws MimeTypeParseException {
-//        URI sourceURI = URI.create("http://claviusontheweb.it:8080/exist/rest//db/clavius/documents/147");
-//        URI contentURI = URI.create("http://claviusontheweb.it:8080/exist/rest//db/clavius/documents/147/147.txt");
-//        documentManager.createSource(sourceURI, new MimeType("text/plain"));
-//        documentManager.setContent(sourceURI,contentURI);
-//        documentManager.inFolder("archivio", sourceURI);
+        URI sourceURI = URI.create("http://claviusontheweb.it:8080/exist/rest//db/clavius/documents/147");
+        URI contentURI = URI.create("http://claviusontheweb.it:8080/exist/rest//db/clavius/documents/147/147.txt");
+        resourceManager.createSource(sourceURI, new MimeType("text/plain"));
+        resourceManager.setContent(sourceURI,contentURI);
+        resourceManager.inFolder("archivio", sourceURI);
         System.err.println("in test");
     }
 }
